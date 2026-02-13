9028b487904e539130a23c91f9b60c708e3645c5
Shashank Vivek <shashank.093028@gmail.com>
2026-02-13 23:31:22 +0530

    Add HelloWorld.java file with sample Java code

---
+++ HelloWorld.java
@@ -0,0 +1,19 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("✅ Hello, World!");
+        System.out.println("This is a sample Java program.");
+        
+        // Simple arithmetic example
+        int num1 = 10;
+        int num2 = 20;
+        int sum = num1 + num2;
+        
+        System.out.println("Sum of " + num1 + " and " + num2 + " is: " + sum);
+        
+        // Loop example
+        System.out.println("\nCounting from 1 to 5:");
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("Count: " + i);
+        }
+    }
+}
